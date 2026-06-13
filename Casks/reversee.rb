8699d086f71289bffec9175c0178a1f1810978c7
Gal Ben Ami <galusben@gmail.com>
2026-06-14 00:32:52 +0300

    Reversee 2.1.0

--- Casks/reversee.rb
+++ Casks/reversee.rb
@@ -1,9 +1,9 @@
 cask "reversee" do
   arch arm: "-arm64", intel: ""
 
-  version "2.0.0"
-  sha256 arm:   "aef5d9debb673fdb49c96869e3ff11593b6d48787e0548978483a8ad9e31b36a",
-         intel: "14aad09715ba1c7542039b6cc12270d20d4c44429b333f6ca5b14fb223ed6140"
+  version "2.1.0"
+  sha256 arm:   "7930cef31b31c21b6b7dc9e93bb8a81abe9848b08b93201aad4ba75241ccf647",
+         intel: "1fd2f04170e0d34d2feb8bf951cfb10938e5d1cf89c9f0516af30e90137c4a01"
 
   url "https://github.com/galusben/reversee/releases/download/v#{version}/Reversee-#{version}#{arch}.dmg"
   name "Reversee"
@@ -13,6 +13,8 @@ cask "reversee" do
   auto_updates true
 
   app "Reversee.app"
+  # Puts a reversee binary on PATH for agents to run it headless.
+  binary "#{appdir}/Reversee.app/Contents/MacOS/Reversee", target: "reversee"
 
   zap trash: [
     "~/Library/Application Support/Reversee",
