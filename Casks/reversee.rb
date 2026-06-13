922de271fdcf5db060eb59a59705b4d6615576c9
Gal Ben Ami <galusben@gmail.com>
2026-06-13 14:32:17 +0300

    Drop unnecessary verified: param

--- Casks/reversee.rb
+++ Casks/reversee.rb
@@ -5,8 +5,7 @@ cask "reversee" do
   sha256 arm:   "aef5d9debb673fdb49c96869e3ff11593b6d48787e0548978483a8ad9e31b36a",
          intel: "14aad09715ba1c7542039b6cc12270d20d4c44429b333f6ca5b14fb223ed6140"
 
-  url "https://github.com/galusben/reversee/releases/download/v#{version}/Reversee-#{version}#{arch}.dmg",
-      verified: "github.com/galusben/reversee/"
+  url "https://github.com/galusben/reversee/releases/download/v#{version}/Reversee-#{version}#{arch}.dmg"
   name "Reversee"
   desc "Reverse-proxy web debugger"
   homepage "https://github.com/galusben/reversee"
